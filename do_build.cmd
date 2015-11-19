5778e2059719c66d2b2be8207d0c64afc3399cbe
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-11-19 20:11:28 +0300

    windres trying to fix

--- do_build.cmd
+++ do_build.cmd
@@ -5,9 +5,9 @@ if NOT "%~1" == "" goto :do_build
 set CHERE_INVOKING=1
 
 rem OK begin
-call "%~0" "%~d0\cygwin\bin" "conemu-cyg-32.exe" "i686-pc-cygwin-" "i686-pc-mingw32-"
-call "%~0" "%~d0\cygwin\bin" "conemu-cyg-64.exe" "x86_64-pc-cygwin-" "i686-w64-mingw32-"
-call "%~0" "%~d0\GitSDK\usr\bin" "conemu-msys2-64.exe"
+call "%~0" "%~d0\cygwin\bin" "conemu-cyg-32.exe" 32 "i686-pc-cygwin-" "i686-pc-mingw32-"
+rem call "%~0" "%~d0\cygwin\bin" "conemu-cyg-64.exe" 64 "x86_64-pc-cygwin-" "x86_64-w64-mingw32-"
+rem call "%~0" "%~d0\GitSDK\usr\bin" 64 "conemu-msys2-64.exe"
 rem OK ends
 
 
@@ -20,12 +20,20 @@ goto :EOF
 
 :do_build
 setlocal
-call cecho /yellow "Using: `%~1`, `%~3g++` and `%~4windres`"
+call cecho /yellow "Using: `%~1`, `%~4g++` and `%~5windres`"
 set "PATH=%~1;%windir%;%windir%\System32;%ConEmuDir%;%ConEmuBaseDir%;"
+
 if exist ConEmuT.res.o ( del ConEmuT.res.o > nul )
-"%~4windres" -i ConEmuT.rc -o ConEmuT.res.o
-"%~3g++" ConEmuT.cpp -o %2 -Xlinker ConEmuT.res.o 2> "%2.log"
+set "RCFLAGS=-F pe-i386"
+if "%~3" == "64" set "RCFLAGS=-F pe-x86-64"
+echo "%~5windres" %RCFLAGS% -i ConEmuT.rc -o ConEmuT.res.o
+"%~5windres" %RCFLAGS% -i ConEmuT.rc -o ConEmuT.res.o 2> "%2.log"
 if errorlevel 1 goto print_errors
+
+echo "%~4g++" ConEmuT.cpp -o %2 -Xlinker ConEmuT.res.o
+"%~4g++" ConEmuT.cpp -o %2 -Xlinker ConEmuT.res.o 2> "%2.log"
+if errorlevel 1 goto print_errors
+
 endlocal
 call sign %2
 call cecho /green "Build succeeded: %2"
