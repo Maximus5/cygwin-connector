b7bd0366068f3f8fdefc5bca183b3e4c2f3d8c79
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-02 22:47:47 +0300

    Simplify `do_build` calls for specific platform
      Now only one argument is required. Options are:
      cygwin32 | cygwin64 | msys1 | msys32 | msys64

--- do_build.cmd
+++ do_build.cmd
@@ -12,11 +12,16 @@ rem For example: do_build cygwin32 conemu-cyg-32.exe
 if NOT "%~1" == "" goto :%~1
 
 rem Start build process for all toolchains
-call "%~0" cygwin32 conemu-cyg-32.exe
-call "%~0" cygwin64 conemu-cyg-64.exe
-call "%~0" msys1 conemu-msys-32.exe
-call "%~0" msys32 conemu-msys2-32.exe
-call "%~0" msys64 conemu-msys2-64.exe
+rem *** conemu-cyg-32.exe
+call "%~0" cygwin32
+rem *** conemu-cyg-64.exe
+call "%~0" cygwin64
+rem *** conemu-msys-32.exe
+call "%~0" msys1
+rem *** conemu-msys2-32.exe
+call "%~0" msys32
+rem *** conemu-msys2-64.exe
+call "%~0" msys64
 
 rem Final cleaning
 if exist ConEmuT.res.o ( del ConEmuT.res.o > nul )
@@ -26,30 +31,35 @@ goto :EOF
 :cygwin32
 set toolchain=%cygwin32toolchain%
 set PATH=%cygwin32toolchain%\bin;%PATH%
+set exe_name=%cygwin32exe%
 call :bit32
 goto :build
 
 :cygwin64
 set toolchain=%cygwin64toolchain%
 set PATH=%cygwin64toolchain%\bin;%PATH%
+set exe_name=%cygwin64exe%
 call :bit64
 goto :build
 
 :msys1
 set toolchain=%msys1toolchain%
 set PATH=%msys1toolchain%\bin;%PATH%
+set exe_name=%msys1exe%
 call :bit32
 goto :build
 
 :msys32
 set toolchain=%msys2x32toolchain%
 set PATH=%msys2x32toolchain%\usr\bin;%PATH%
+set exe_name=%msys2x32exe%
 call :bit32
 goto :build
 
 :msys64
 set toolchain=%msys2x64toolchain%
 set PATH=%msys2x64toolchain%\usr\bin;%PATH%
+set exe_name=%msys2x64exe%
 call :bit64
 goto :build
 
@@ -67,30 +77,30 @@ goto :EOF
 
 :build
 setlocal
-call cecho /yellow "Using: `%toolchain%` for `%~2` %DIRBIT%bit"
+call cecho /yellow "Using: `%toolchain%` for `%exe_name%` %DIRBIT%bit"
 
 if exist ConEmuT.res.o ( del ConEmuT.res.o > nul )
 echo Compiling resources
-windres %RCFLAGS% -i ConEmuT.rc -o ConEmuT.res.o 2> "%~2.log"
+windres %RCFLAGS% -i ConEmuT.rc -o ConEmuT.res.o 2> "%exe_name%.log"
 if errorlevel 1 goto print_errors
 
 echo Compiling code and linking
-g++ %LOGGING% ConEmuT.cpp -o %2 -Xlinker ConEmuT.res.o -mconsole -m%DIRBIT% %NO_DEBUG% 2> "%~2.log"
+g++ %LOGGING% ConEmuT.cpp -o %exe_name% -Xlinker ConEmuT.res.o -mconsole -m%DIRBIT% %NO_DEBUG% 2> "%exe_name%.log"
 if errorlevel 1 goto print_errors
 
 if NOT "%sign_code%" == "YES" goto skip_sign
-echo Signing `%~2`
-call sign "%~2" > nul
+echo Signing `%exe_name%`
+call sign "%exe_name%" > nul
 :skip_sign
 
 endlocal
-call cecho /green "Build succeeded: %2"
+call cecho /green "Build succeeded: %exe_name%"
 goto :EOF
 
 :print_errors
 set "err_msg=Build failed: %errorlevel%"
 echo --- build errors (begin)
-type "%2.log"
+type "%exe_name%.log"
 echo --- build errors (end)
 call cecho "%err_msg%"
 rem exit /B 99
